287ced85c104467fc8b6abe80db34b104bc4fb58
Oshannk <oshanmuththettupita@outlook.com>
2022-11-11 03:21:56 +0530

    itemCount added

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -3,13 +3,20 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import WelcomeScreen from '../screens/WelcomeScreen';
 import HomeScreen from '../screens/HomeScreen';
-import {StatusBar, TouchableOpacity} from 'react-native';
+import {StatusBar, StyleSheet, TouchableOpacity, View} from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
 import Colors from '../constants/Colors';
 import CartScreen from '../screens/CartScreen';
+import {ApplicationState} from '../redux/reducers';
+import {useSelector} from 'react-redux';
+import {Text} from 'react-native-elements';
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
+  const cart = useSelector((state: ApplicationState) => {
+    return state.robots.cart;
+  });
+
   return (
     <NavigationContainer>
       <StatusBar backgroundColor={Colors.primaryColor} />
@@ -35,6 +42,11 @@ const AppNavigator = () => {
               <TouchableOpacity
                 style={{marginRight: 20}}
                 onPress={() => navigation.navigate('CartScreen')}>
+                {cart.length > 0 && (
+                  <View style={styles.itemCount}>
+                    <Text style={styles.count}>{cart.length}</Text>
+                  </View>
+                )}
                 <Feather name="shopping-cart" size={25} color="#403F3F" />
               </TouchableOpacity>
             ),
@@ -62,3 +74,19 @@ const AppNavigator = () => {
 };
 
 export default AppNavigator;
+
+const styles = StyleSheet.create({
+  count: {fontSize: 12, color: Colors.secondaryColor},
+  itemCount: {
+    position: 'absolute',
+    backgroundColor: '#fff',
+    width: 18,
+    height: 18,
+    zIndex: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    top: -6,
+    right: -9,
+  },
+});
